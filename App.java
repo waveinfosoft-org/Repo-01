305f0f6aca2cd7843abfea17f42e637aa342735b
dev1 <dev1@gmail.com>
2019-07-29 09:52:36 +0530

    modified app.java

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
-public class App1
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "new Branch creation and merging" );
+       System.out.println( "hello world" );
+     
     }
 }
